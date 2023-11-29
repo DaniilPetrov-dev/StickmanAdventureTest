c43cb08f769fbcf86219a7ad47f834eef1b4e659
DaniilPetrov-dev <ichitrigerfingern1ggas@gmail.com>
2023-11-30 00:55:37 +0300

    Add Builds(0.14,0.15,0.16,0.17,0.18,0.19)

---
+++ Build_0.15/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-StickmanAdventure-0.1";
+const contentToCache = [
+    "Build/Bulid_0.15.loader.js",
+    "Build/Bulid_0.15.framework.js.unityweb",
+    "Build/Bulid_0.15.data.unityweb",
+    "Build/Bulid_0.15.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
